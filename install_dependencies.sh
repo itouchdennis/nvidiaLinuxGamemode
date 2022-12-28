ef3e5591df435ae0d67ea33676703251b5bcac6e
Dennis A <itouchdennis@gmail.com>
2022-12-28 12:12:02 +0100

    Fixed typo

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,6 +12,6 @@ chmod +x nvidia_pipeline_off.sh
 chmod +x nvidia_pipeline_on.sh
 chmod +x autostart/nvidia_autostart.sh
 cp nvidia_pipeline_off.sh ~/scripts/nvidia_pipeline_off.sh
-cp nvidia_pipeline_on.sh ~/scripts/nvidia_pipeline_off.sh
+cp nvidia_pipeline_on.sh ~/scripts/nvidia_pipeline_on.sh
 
 cp .config/.gamemode.ini ~/.config/.gamemode.ini
